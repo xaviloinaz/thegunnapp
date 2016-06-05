b9f4956605c3251a4b6e5f0c14c5390e1bd1bbf8
Xavi Loinaz <Xavi@xavis-macbook-pro.local>
2016-06-05 14:34:04 -0700

    Initial Commit

---
+++ The Gunn App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  The Gunn App
+//
+//  Created by Xavi Loinaz on 6/5/16.
+//  Copyright © 2016 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Gunn App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  The Gunn App
+//
+//  Created by Xavi Loinaz on 6/5/16.
+//  Copyright © 2016 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
