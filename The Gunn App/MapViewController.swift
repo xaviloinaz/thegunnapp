70e5ff7a66acdfd61e0dd1f9679a1bb47eb4d695
Xavi Loinaz <Xavi@xavis-macbook-pro.local>
2016-08-02 16:55:23 -0700

    figuring out the map stuff

--- The Gunn App/MapViewController.swift
+++ The Gunn App/MapViewController.swift
@@ -9,27 +9,19 @@
 import Foundation
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var mapImage: UIImageView!
     
     @IBOutlet weak var scrollView: UIScrollView!
-    
-    @IBOutlet weak var mapLeadingConstraint: NSLayoutConstraint!
-    
-    @IBOutlet weak var mapTrailingConstraint: NSLayoutConstraint!
-    
-    @IBOutlet weak var mapBottomConstraint: NSLayoutConstraint!
-    
-    @IBOutlet weak var mapTopConstraint: NSLayoutConstraint!
-    
-    
+
+    @IBOutlet weak var gunnMap: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController!.navigationBar.barTintColor = UIColor.redColor()
-        mapImage.image = UIImage(named: "Gunn Campus Map 09-27-15")
         // Do any additional setup after loading the view, typically from a nib.
+        self.scrollView.minimumZoomScale = 0.4
+        self.scrollView.maximumZoomScale = 6.0
     }
     
     override func didReceiveMemoryWarning() {
@@ -38,25 +30,9 @@ class MapViewController: UIViewController {
     }
     
     
-    private func updateMinZoomScaleForSize(size: CGSize) {
-        let widthScale = size.width / mapImage.bounds.width
-        let heightScale = size.height / mapImage.bounds.height
-        let minScale = min(widthScale, heightScale)
-        
-        scrollView.minimumZoomScale = 1.0
-        
-        scrollView.zoomScale = 10.0
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        updateMinZoomScaleForSize(view.bounds.size)
-    }
-}
-
-extension MapViewController: UIScrollViewDelegate {
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return mapImage
+        return self.gunnMap
     }
+    
+    
 }
