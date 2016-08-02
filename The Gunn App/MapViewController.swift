9a6a5619853e09290672de7a1ff7e5bca0e5c62a
Xavi Loinaz <Xavi@xavis-macbook-pro.local>
2016-08-02 16:10:02 -0700

    trying to figure out how to zoom in and out of map image

--- The Gunn App/MapViewController.swift
+++ The Gunn App/MapViewController.swift
@@ -11,9 +11,24 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapImage: UIImageView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var mapLeadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var mapTrailingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var mapBottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var mapTopConstraint: NSLayoutConstraint!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController!.navigationBar.barTintColor = UIColor.redColor()
+        mapImage.image = UIImage(named: "Gunn Campus Map 09-27-15")
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -21,5 +36,27 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    private func updateMinZoomScaleForSize(size: CGSize) {
+        let widthScale = size.width / mapImage.bounds.width
+        let heightScale = size.height / mapImage.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = 1.0
+        
+        scrollView.zoomScale = 10.0
+    }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        updateMinZoomScaleForSize(view.bounds.size)
+    }
+}
+
+extension MapViewController: UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return mapImage
+    }
 }
