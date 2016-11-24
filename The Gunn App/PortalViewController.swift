cf56678bd89ca7427496efd7b60c6a57f0554a64
Xavi Loinaz <x.e.loinaz@gmail.com>
2016-11-23 20:35:37 -0800

    pretty close to finishing flextime barcode scanner

--- The Gunn App/PortalViewController.swift
+++ The Gunn App/PortalViewController.swift
@@ -9,8 +9,9 @@
 import Foundation
 import UIKit
 
-class PortalViewController: UIViewController {
+class PortalViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var textbox: UITextField!
     @IBOutlet weak var barcode: UIImageView!
     @IBAction func myPAUSDButton() {
         if let url = NSURL(string: "https://id.pausd.org/arms/m") {
@@ -19,10 +20,31 @@ class PortalViewController: UIViewController {
             
         }
     }
+    
+    @IBAction func editingchanged(sender: UITextField) {
+        print("editingchanged")
+        if(textbox.text?.characters.count >= 8){
+            print(textbox.text)
+            textFieldShouldReturn(textbox);
+            barcode.image = generateBarcodeFromString(textbox.text!)
+        }
+    }
+    @IBAction func valuechanged(sender: UITextField) {
+        print("valuechanged")
+    }
+
+    
+
+    @IBAction func editingdidend(sender: AnyObject) {
+        print("editingdidend")
+        barcode.image = generateBarcodeFromString(textbox.text!)
+    }
+
+    
     func generateBarcodeFromString(string: String) -> UIImage? {
         let data = string.dataUsingEncoding(NSASCIIStringEncoding)
         
-        if let filter = CIFilter(name: "CICode128BarcodeGenerator") {
+        if let filter = CIFilter(name: "CICode39BarcodeGenerator") {
             filter.setValue(data, forKey: "inputMessage")
             let transform = CGAffineTransformMakeScale(3, 3)
             
@@ -41,7 +63,9 @@ class PortalViewController: UIViewController {
         navigationController!.navigationBar.barTintColor = specialColor
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        barcode.image = generateBarcodeFromString("95016633");
+//        barcode.image = generateBarcodeFromString("95016633");
+        self.textbox.delegate = self;
+        self.textbox.keyboardType = UIKeyboardType.NumberPad;
 
     }
     
@@ -50,6 +74,20 @@ class PortalViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        print("textfield")
+        
+        let aSet = NSCharacterSet(charactersInString:"0123456789").invertedSet
+        let compSepByCharInSet = string.componentsSeparatedByCharactersInSet(aSet)
+        let numberFiltered = compSepByCharInSet.joinWithSeparator("")
+        return string == numberFiltered
+        
+    }
+
+    
     
 }
-
